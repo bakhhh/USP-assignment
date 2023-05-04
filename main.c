c185684b2be3fcd5c986524c7e786729609abf87
Sohail Bakhshi <sohailb@Sohails-MBP.modem>
2023-05-04 22:28:46 +0800

    done first bit

--- main.c
+++ main.c
@@ -7,27 +7,42 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-void listFiles(){
+int listFiles(){
         struct dirent *dp;
         DIR* directory = opendir(".");
         char file[200];
         dp = readdir(directory);
         int n;
+        int stringLength;
+        int countFiles=0;
          while(dp!=NULL){
              if (dp->d_type == 8){
-                strcpy(file,"./");
-                strcat(file, dp->d_name);
-                strcat(file, "\n");
-                n = strlen(file);
-                write(1, file, n);
+                stringLength = strlen(dp->d_name);
+                for (int i=stringLength-1;i>0;i--)
+                {
+                    if(dp->d_name[i]== '.'){
+                        if (strcmp(&dp->d_name[i], ".usp") == 0){
+                            strcpy(file,"./");
+                            strcat(file, dp->d_name);
+                            strcat(file, "\n");
+                            n = strlen(file);
+                            write(1, file, n);
+                            countFiles++;
+                    }
+                }
+                }
+
              }
             dp = readdir(directory);
          }
         closedir(directory);
+        return countFiles;
 }
 
 int main(){
-    listFiles();
+    int count;
+    count = listFiles();
+    printf("number of usp files: %d\n",count);
 
 
     return 0;
