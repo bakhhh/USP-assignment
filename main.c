07f640103202bfeaf8f6cec75f94a93ec4931355
Sohail Bakhshi <sohailb@Sohails-MBP.modem>
2023-05-08 21:27:15 +0800

    fixed

--- main.c
+++ main.c
@@ -7,28 +7,16 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-
-
-char **create2DArray(int row, int col)
+char **create2DArray(int row, int col) // UCP reference
 {
     int i, j;
     char **array;
     array = (char **)malloc(sizeof(char *) * row);
-    /* malloc columns for each row */
     for (i = 0; i < row; i++)
     {
         array[i] = (char *)malloc(sizeof(char) * col);
     }
     return array;
-}
-
-void createPipes(int numberOfFiles)
-{
-    int pipeFd[numberOfFiles][2];
-    for (int i = 0; i < numberOfFiles; i++)
-    {
-        pipe(pipeFd[i]);
-    }
 }
 
 char **listFiles(int *countFiles)
@@ -59,13 +47,10 @@ char **listFiles(int *countFiles)
 
 int main()
 {
-    char **filename;
     int countFiles = 0;
+    char **filename = listFiles(&countFiles);
     int pid;
     int i;
-    char buffer[200];
-    
-    filename = listFiles(&countFiles);
 
     int writingFilenamePipe[countFiles][2];
     int writingResultsPipe[countFiles][2];
@@ -83,135 +68,120 @@ int main()
         if (pid > 0)
         {
             int n = strlen(filename[i]);
-            char result[30];
+            char *result;
+            int resN;
             close(writingFilenamePipe[i][0]);
             write(writingFilenamePipe[i][1], filename[i], n);
-            // close(writingFilenamePipe[i][1]);
+            close(writingFilenamePipe[i][1]);
 
             close(writingResultsPipe[i][1]);
-            
-            int fd2 = open("results.txt",O_WRONLY | O_CREAT |O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-            int size = read(writingResultsPipe[i][0], result, sizeof(result));
-            result[size] = '\0'; 
-            printf("parent result= %s\n",result);
+            int fd2 = open("results.txt", O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+            int size = read(writingResultsPipe[i][0], result, 30);
+            result[size] = '\0';
             write(fd2, result, strlen(result));
             write(fd2, "\n", 1);
-
-
-
             close(fd2);
-            // close(writingResultsPipe[i][0]);
-
+            close(writingResultsPipe[i][0]);
+            wait(NULL);
         }
         else if (pid == 0)
         {
+            char buffer[200];
             close(writingFilenamePipe[i][1]);
-
             int size = read(writingFilenamePipe[i][0], buffer, 200);
-            // write(1, buffer, size)
             close(writingFilenamePipe[i][0]);
-            printf("Child process received filename: %s\n", buffer);
-                        
 
             int fd1 = open(buffer, O_RDONLY);
             int line = 0;
-            int num1=0;
-            int num2=0;
+            int num1 = 0;
+            int num2 = 0;
             char buf;
             char operator;
-            int result=0;
+            int result = 0;
             char id[20];
             char output[30];
-            int index =0;
-        
-            
+            int index = 0;
+
             while (read(fd1, &buf, 1))
             {
-                if (buf== '\n'){
+                if (buf == '\n')
+                {
                     line++;
                 }
-                if (line==0){
-                    
-                    id[index] =buf;
-                    index ++;
+                if (line == 0)
+                {
+                    id[index] = buf;
+                    index++;
                 }
-                if (line==1)
+                if (line == 1)
+                {
+                    if (buf >= '0' && buf <= '9')
+                    { 
+                        num1 = num1 * 10 + (buf - '0');
+                    }
+                }
+                if (line == 2)
                 {
                     {
-                        num1=num1*10+atoi(&buf);   
+                        operator= buf;
                     }
                 }
-                if (line==2){
+                if (line == 3)
+                {
+                    if (buf >= '0' && buf <= '9')
                     {
-                        operator = buf;
+                        num2 = num2 * 10 + (buf - '0');
                     }
                 }
-                if (line==3){
-                    {
-                        num2=num2*10+atoi(&buf);
-                    }
-                }
-                
+            }
+            id[index] = '\0';
+            printf("num2 %d\n", num2);
 
-              
-                }
+            if (strncmp(&operator, "+", 1) == 0)
+            {
+                result = num1 + num2;
+                char str[10];
+                sprintf(str, "%d", result);
+                strcat(output, id);
+                strcat(output, ":");
+                strcat(output, str);
+            }
+            if (strncmp(&operator, "*", 1) == 0)
+            {
+                result = num1 * num2;
+                char str[10];
+                sprintf(str, "%d", result);
+                strcat(output, id);
+                strcat(output, ":");
+                strcat(output, str);
+            }
+            if (strncmp(&operator, "-", 1) == 0)
+            {
+                result = num1 - num2;
+                char str[10];
+                sprintf(str, "%d", result);
+                strcat(output, id);
+                strcat(output, ":");
+                strcat(output, str);
+            }
+            if (strncmp(&operator, "/", 1) == 0)
+            {
+                result = num1 / num2;
+                char str[10];
+                sprintf(str, "%d", result);
+                strcat(output, id);
+                strcat(output, ":");
+                strcat(output, str);
+            }
+            output[strlen(output)] = '\0';
+            printf("output %s %lu\n", output, strlen(output));
+            int n = strlen(output);
             close(writingResultsPipe[i][0]);
-            
-            // printf("id= %s\n", id);
-
-            if (strncmp(&operator, "+", 1)==0)
-            {
-                result=num1+num2;
-                char str[10];
-                sprintf(str,"%d",result);
-                strcat(output,id);
-                strcat(output,":");
-                strcat(output,str);
-                write(writingResultsPipe[i][1], output, strlen(output));
-            }
-            if (strncmp(&operator,"*",1)==0)
-            {
-                result=num1*num2;
-                char str[10];
-                sprintf(str,"%d",result);
-                strcat(output,id);
-                strcat(output,":");
-                strcat(output,str);
-                write(writingResultsPipe[i][1], output, strlen(output));
-            }
-            if (strncmp(&operator,"-",1)==0)
-            {
-                result=num1-num2;
-                char str[10];
-                sprintf(str,"%d",result);
-                strcat(output,id);
-                strcat(output,":");
-                strcat(output,str);
-                write(writingResultsPipe[i][1], output, strlen(output));
-            }
-            if (strncmp(&operator,"/",1)==0)
-            {
-                result=num1/num2;
-                char str[10];
-                sprintf(str,"%d",result);
-                strcat(output,id);
-                strcat(output,":");
-                strcat(output,str);
-                write(writingResultsPipe[i][1], output, strlen(output));
-            }
-            // printf("result= %s\n", output);
-            // char sendToPipe[strlen(output)];
-            // strcpy(sendToPipe,output);
-            printf("result= %s length %lu\n", output, strlen(output));
-            // write(writingResultsPipe[i][1], output, strlen(output));
-            // close(writingResultsPipe[i][1]);
-            
+            write(writingResultsPipe[i][1], output, n);
+            close(writingResultsPipe[i][1]);
             close(fd1);
-            
-
-            return 0;
+            exit(0);
         }
-        wait(NULL);
     }
 
     for (i = 0; i < 20; i++)
